b1e287c6820c8ce813cfda6004a48c5b8b99ef52
expl0it <solovey.anton@gmail.com>
2019-01-02 13:55:49 +0300

    fixed koken import sql expression

--- koken-import-wp-posts.sql
+++ koken-import-wp-posts.sql
@@ -1,33 +1,27 @@
-INSERT INTO
-likeafreed_koken.koken_text (id, title, content, excerpt, created_on, published_on, modified_on, published, page_type, internal_id)
-
+INSERT INTO likeafreed_koken.koken_text(
+    id,
+    title,
+    content,
+    excerpt,
+    created_on,
+    published_on,
+    modified_on,
+    published,
+    page_type,
+    internal_id
+)
 SELECT
-    p.id,
-    p.post_title,
-    p.post_content,
-    p.post_excerpt,
-    unix_timestamp(p.post_date),
-    unix_timestamp(p.post_date),
-    unix_timestamp(p.post_modified),
+    ID,
+    post_title,
+    post_content,
+    post_excerpt,
+    UNIX_TIMESTAMP(post_date),
+    UNIX_TIMESTAMP(post_date),
+    UNIX_TIMESTAMP(post_modified),
     1,
     0,
-    md5(CONCAT(p.post_title,p.post_date)),
-    p.post_name,
-    c.name,
-    GROUP_CONCAT(t.`name`)
-FROM wp_posts p
-JOIN wp_term_relationships cr
-    on (p.`id`=cr.`object_id`)
-JOIN wp_term_taxonomy ct
-    on (ct.`term_taxonomy_id`=cr.`term_taxonomy_id`
-    and ct.`taxonomy`='category')
-JOIN wp_terms c on
-    (ct.`term_id`=c.`term_id`)
-JOIN wp_term_relationships tr
-    on (p.`id`=tr.`object_id`)
-JOIN wp_term_taxonomy tt
-    on (tt.`term_taxonomy_id`=tr.`term_taxonomy_id`
-    and tt.`taxonomy`='post_tag')
-JOIN wp_terms t
-    on (tt.`term_id`=t.`term_id`)
-GROUP BY p.id
+    MD5(CONCAT(post_title, post_date))
+FROM
+    likeafreed_wordpress.wp_posts
+WHERE
+    post_status = 'publish' AND post_content != '' AND post_type = 'post'
